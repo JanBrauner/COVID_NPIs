4c570995fde438a3a7d127332641d50e3e87e98f
Tomas Gavenciak <gavento@gmail.com>
2020-04-21 17:50:43 +0200

    WIP - Make smart summing algorithm

--- epimodel/algorithms/generic.py
+++ epimodel/algorithms/generic.py
@@ -1,19 +1,23 @@
+from typing import Union
+
 import numpy as np
 import pandas as pd
 
-from ..regions import RegionDataset, Region, Level
+from ..regions import Level, Region, RegionDataset
 
 
 def aggregate_sum(
-    series: pd.Series,
+    data: Union[pd.Series, pd.DataFrame],
     rds: RegionDataset,
     overwrite_parents=False,
     atol=0.0,
     rtol=1e-5,
-    root="W",
 ):
     """
-    Aggregate numbers in the Series creating sums up the tree, returning a new Series.
+    Aggregate numbers in `data` indexed by `Code` creating sums up the tree, returning a new Series.
+
+    Operates on all columns of `data`. Works with multiindex if `Code` is level 0.
+    In multiindex, missing secondary indecies are assumed to be 0 rather than NaN.
 
     NaNs are propagated upwards in the sums. Missing entries are assumed to be zero, but
     only regions with data or data in their descendant are present in the output.
@@ -25,44 +29,71 @@ def aggregate_sum(
       `ValueError` on mismatch.
     """
 
-    assert isinstance(series, pd.Series)
+    def idxL0(d):
+        "Return 0-level index of Series or DF"
+        return d.index.levels[0] if isinstance(d.index, pd.MultiIndex) else d.index
+
+    # List of (multi)index levels
+    df_levels = (
+        list(range(len(data.index.levels)))
+        if isinstance(data.index, pd.MultiIndex)
+        else 0
+    )
+
+    assert isinstance(data, (pd.Series, pd.DataFrame))
     level_regions = {l: set() for l in Level}
-    for rc in series.index:
-        r = rds[rc]
+    for r in [rds[rc] for rc in idxL0(data)]:
         level_regions[r.Level].add(r)
 
     # When inspecting a region, s2[r.Code] already contains sum of values from
     # children, if any pushed upwards. This value may be NaN
-    s2 = pd.Series(dtype=series.dtype)
+    d2 = data.loc[()]
 
     # From the smallest levels to largest
     for l in sorted(Level):
+        print("\n", l, idxL0(data), idxL0(d2))
+        addrows = []
+
         for r in level_regions[l]:
-            if r.Code not in s2.index:
+            # print(r, d2, idxL0(d2))
+            if r.Code not in idxL0(d2):
                 # Leaf node - just copy from the original series (must be there)
-                s2[r.Code] = series[r.Code]
-            elif r.Code not in series.index:
+                val = data.loc[[r.Code]]
+                addrows.append(val)
+            elif r.Code not in idxL0(data):
                 # Data from children but not present in original - do nothing
-                pass
+                val = d2.loc[[r.Code]]
             else:
                 # Data in both. If differs too much and not overwriting, raise.
                 # Otherwise do nothing.
-                diff = np.abs(s2[r.Code] - series[r.Code])
                 if overwrite_parents or np.isclose(
-                    s2[r.Code], series[r.Code], rtol=rtol, atol=atol, equal_nan=True
+                    d2.loc[r.Code],
+                    data.loc[r.Code],
+                    rtol=rtol,
+                    atol=atol,
+                    equal_nan=True,
                 ):
-                    pass
+                    val = d2.loc[[r.Code]]
                 else:
                     raise ValueError(
-                        f"Region {r!r}: children aggregate {s2[r.Code]} differs "
-                        f"from original value {series[r.Code]}"
+                        f"Region {r!r}: children aggregate {d2.loc[r.Code]} differs "
+                        f"from original value {data.loc[r.Code]}"
                     )
             # Propagate to parent, and add parent to the queue
             if r.parent:
+                # Replace r with r.parent in d2n
+                val = val.copy()
+                val.index = val.index.map(
+                    lambda x: (r.parent.Code,) + x[1:]
+                    if isinstance(val.index, pd.MultiIndex)
+                    else r.parent.Code
+                )
+                addrows.append(val)
                 level_regions[r.parent.Level].add(r.parent)
-                pcode = r.parent.Code
-                if pcode not in s2.index:
-                    s2[pcode] = 0.0
-                s2[pcode] += s2[r.Code]
+        d2 = (
+            pd.concat([d2] + addrows)
+            .groupby(level=df_levels)
+            .apply(lambda x: x.sum(min_count=len(x)))
+        )
 
-    return s2.sort_index()
+    return d2.sort_index()
--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -36,7 +36,7 @@ def test_distribute_down(regions_gleam):
     assert s["W-NA"] == 9e5
 
 
-def test_aggregate_sum(regions):
+def test_aggregate_sum_series(regions):
     s = pd.Series([100.0, 200.0, 3000.0, np.nan], index=["CZ", "PL", "CA", "CN"])
     s2 = algorithms.aggregate_sum(s, regions)
 
@@ -54,3 +54,32 @@ def test_aggregate_sum(regions):
 
     s2 = algorithms.aggregate_sum(s, regions, overwrite_parents=False, atol=11.0)
     assert s2["W-EU"] == pytest.approx(300)
+
+
+def test_aggregate_sum_df(regions):
+    df = pd.DataFrame(
+        {"a": [100.0, 200.0, 3000.0, np.nan], "b": [10, 10, np.nan, 20.0]},
+        index=["CZ", "PL", "CA", "CN"],
+    )
+    df2 = algorithms.aggregate_sum(df, regions)
+
+    assert_almost_equal(df2.loc["W"], [np.nan, np.nan])
+    assert_almost_equal(df2.loc["W-AS"], [np.nan, 20.0])
+    assert_almost_equal(df2.loc["W-NA"], [3000, np.nan])
+
+    df = pd.DataFrame(
+        [
+            ["CZ", "a", 1],
+            ["CZ", "b", 2],
+            ["PL", "b", np.nan],
+            ["PL", "c", 3],
+            ["CN", "c", np.nan],
+        ],
+        columns=["Code", "Bar", "Foo"],
+    ).set_index(["Code", "Bar"])
+    df2 = algorithms.aggregate_sum(df, regions)
+
+    assert_almost_equal(df2.loc[("W", "a")], 1)
+    assert_almost_equal(df2.loc[("W", "b")], np.nan)
+    assert_almost_equal(df2.loc[("W", "c")], np.nan)
+    assert_almost_equal(df2.loc[("W-EU", "c")], 3)
