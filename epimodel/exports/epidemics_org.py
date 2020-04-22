ec021413718bc9b3a38c96a25fe7d96a00ad5367
Mathijs Henquet <mathijs.henquet@gmail.com>
2020-04-22 21:22:56 +0200

    Make CurrentEstimates flexible

--- epimodel/exports/epidemics_org.py
+++ epimodel/exports/epidemics_org.py
@@ -14,6 +14,7 @@ from tqdm import tqdm
 
 from ..gleam import Batch
 from ..regions import Region
+import epimodel
 
 log = logging.getLogger(__name__)
 
@@ -44,7 +45,7 @@ class WebExport:
     def new_region(
         self,
         region,
-        current_estimate: int,
+        current_estimate: Optional[pd.DataFrame],
         groups,
         models: pd.DataFrame,
         initial: pd.DataFrame,
@@ -98,7 +99,7 @@ class WebExportRegion:
     def __init__(
         self,
         region: Region,
-        current_estimate: int,
+        current_estimate: Optional[pd.DataFrame],
         groups,
         models: pd.DataFrame,
         initial: pd.DataFrame,
@@ -258,7 +259,12 @@ class WebExportRegion:
             "CountryCodeISOa3",
             "SubdivisionCode",
         ]:
-            d[n] = None if pd.isnull(self.region[n]) else self.region[n]
+            if not pd.isnull(self.region[n]):
+                d[n] = self.region[n]
+
+        if self.current_estimate is not None:
+            d["CurrentEstimate"] = self.current_estimate.to_dict()
+
         return d
 
 
@@ -366,6 +372,8 @@ def analyze_data_consistency(
         df[source_name] = pd.Series(True, index=ixs)
     df = df.fillna(False)
 
+    log.info("Modelled %s", set(codes["models"]).difference(export_regions))
+
     log.info("Total data availability, number of locations: %s", df.sum().to_dict())
     log.info("Export requested for %s regions: %s", len(export_regions), export_regions)
 
@@ -463,7 +471,7 @@ def process_export(args) -> None:
     initial_df = batch.hdf["initial_compartments"]
     cummulative_active_df = batch.get_cummulative_active_df()
 
-    estimates_df = pd.read_csv(args.estimates, index_col="Name")
+    estimates_df = epimodel.read_csv_smart(args.estimates, args.rds)
 
     rates_df: pd.DataFrame = pd.read_csv(
         rates, index_col="Code", keep_default_na=False, na_values=[""]
@@ -510,18 +518,9 @@ def process_export(args) -> None:
         m49 = int(reg["M49Code"])
         iso3 = reg["CountryCodeISOa3"]
 
-        # TODO clean this up
-        initial_estimate = estimates_df[estimates_df.index.isin(reg.AllNames)][
-            "Infectious_mean"
-        ]
-        if initial_estimate.empty:
-            log.error("No estimate found for country code: %s. Skipping", code)
-            continue
-        initial_estimate = int(initial_estimate)
-
         ex.new_region(
             reg,
-            initial_estimate,
+            get_df_else_none(estimates_df, code),
             args.config["groups"],
             cummulative_active_df.xs(key=code, level="Code").sort_index(level="Date"),
             get_df_else_none(initial_df, code),
